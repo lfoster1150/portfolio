8808418c3cd27e63e02ec2f83cc4d035ccd9865b
lfoster1150 <lfoster1150@gmail.com>
2021-10-23 23:07:34 -0500

    MVP

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import PageTitle from './PageTitle'
+import { Container } from 'react-bootstrap'
 
 const Contact = () => {
   return (
     <div className="page contact" id="contact">
-      <PageTitle title="CONTACT INFO" />
+      <Container>
+        <h2 className="skills-header">Contact</h2>
+        <Container className="links">
+          <a
+            className="nav-contact contact-icon"
+            href="https://github.com/lfoster1150"
+            target="_blank"
+          >
+            <i class="devicon-github-original"></i>
+            GitHub
+          </a>
+          <a
+            className="nav-contact contact-icon"
+            href="https://www.linkedin.com/in/luke-foster11/"
+            target="_blank"
+          >
+            <i class="devicon-linkedin-plain"></i>
+            LinkedIn
+          </a>
+          <a
+            className="nav-contact contact-icon"
+            href="mailto:lfoster1150@gmail.com"
+          >
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            lfoster1150@gmail.com
+          </a>
+          <label className="number contact-icon">
+            <i class="fa fa-mobile"></i>
+            (512) - 888 - 5885
+          </label>
+        </Container>
+      </Container>
     </div>
   )
 }
